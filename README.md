5d2b562cba4d74fadcbff3aef9d7fd555144909c
eionschreiber <eionschreiber@gmail.com>
2017-03-21 15:12:17 -0500

    Initial Commit (files moved from original)

---
+++ README.md
@@ -0,0 +1,2 @@
+# prop_storm
+Website made for a replica prop business as a portfolio site. This site will be made with Wordpress and a custom theme. 
---
+++ wp-content/themes/prop_storm_theme/header.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Prop Storm</title>
+    <meta name="Description" content="Prop Storm home page">
+    <meta name="Author" content="Eion Schreiber">
+  </head>
+  <body>
